74072e40f160ea2c784b90933d74e001af5265bc
esin <esinemer@gmail.com>
2017-10-25 20:19:29 +0300

    Allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ygeoaw2ubc4ljzn7hea0ig$^@hx-erf^qprd2+9e8mmqzvu5+x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'esinduction.pythonanywhere.com']
 
 
 # Application definition
